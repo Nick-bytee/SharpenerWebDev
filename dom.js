d242b80f411fdab89c0252ed0a388c55212bb92b
nick-bytee <nikhilkaushik2228@gmail.com>
2023-04-16 15:23:43 +0530

    Changes in List items style

--- dom.js
+++ dom.js
@@ -1,5 +1,10 @@
 const header = document.getElementById("main-header")
 const item1 = document.getElementById("item1")
 header.style.borderBottom = 'solid 3px black'
-item1.style.fontWeight = '900'
-item1.style.color = 'green'
+//item1.style.fontWeight = '900'
+//item1.style.color = 'green'
+const list = document.getElementsByClassName("list-group-item")
+list[2].style.backgroundColor = 'green'
+for(let i = 0; i<list.length; i++){
+    list[i].style.fontWeight = '900'
+}
