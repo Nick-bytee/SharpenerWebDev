3f1a477023c6efe91d1bed8014696dd8f59bb74f
nick-bytee <nikhilkaushik2228@gmail.com>
2023-04-16 15:34:11 +0530

    Adding li element and editing it

--- dom.js
+++ dom.js
@@ -8,3 +8,6 @@ list[2].style.backgroundColor = 'green'
 for(let i = 0; i<list.length; i++){
     list[i].style.fontWeight = '900'
 }
+const li = document.getElementsByTagName('li')
+li[4].style.fontWeight = '900'
+li[4].style.backgroundColor = 'grey'
