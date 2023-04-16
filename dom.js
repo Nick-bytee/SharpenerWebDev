bcdc1a486dbe75b02681c9123f5af5f4c3503b92
nick-bytee <nikhilkaushik2228@gmail.com>
2023-04-16 14:48:20 +0530

    added two more files

---
+++ dom.js
@@ -0,0 +1,5 @@
+const header = document.getElementById("main-header")
+const item1 = document.getElementById("item1")
+header.style.borderBottom = 'solid 3px black'
+item1.style.fontWeight = '900'
+item1.style.color = 'green'
