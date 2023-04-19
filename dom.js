c4664ebb7d3e1f36fd322d2b88527097818dd351
nick-bytee <nikhilkaushik2228@gmail.com>
2023-04-20 00:53:28 +0530

    Search through description added

--- dom.js
+++ dom.js
@@ -1,48 +1,43 @@
 var form = document.getElementById('addForm')
 var items = document.getElementById('items')
+var filter = document.getElementById('filter')
+
 
 form.addEventListener('submit', additem)
 
 items.addEventListener('click', deleteitem)
-
+filter.addEventListener('keyup', search_ele)
 //additem function
 function additem(e){
     e.preventDefault();
-    console.log('Test')
     //create new li element
     var li = document.createElement('li');
 
+    var details = document.getElementById('desc').value;
+
     //get input data
     var item = document.getElementById('item').value;
-
     //add class to li
     li.className = 'list-group-item'
-
     // add textnode to li element
+    li.appendChild(document.createTextNode(details))
     li.appendChild(document.createTextNode(item))
-
     //create delete button
     var deletebtn = document.createElement('button')
-
     //set button class
     deletebtn.className = 'btn btn-danger btn-sm float-right delete' 
-
     //edit button
     var editbtn = document.createElement('button')
-
     //edit button class
-    editbtn.className = 'btn btn-danger btn-sm float-right edit'
+    editbtn.className = 'btn btn-light btn-sm float-right edit'
     editbtn.appendChild(document.createTextNode('Edit'))
-
-    deletebtn.appendChild(document.createTextNode('Delete'))
-
-    li.appendChild(editbtn)
+    deletebtn.appendChild(document.createTextNode('Del'))
     li.appendChild(deletebtn)
-
+    li.appendChild(editbtn)
     //add item to list
     items.appendChild(li);
+    console.log(li.children)
 
-    
 }
 function deleteitem(a){
     if(a.target.classList.contains('delete')){
@@ -52,3 +47,19 @@ function deleteitem(a){
         };
     };
 };
+
+function search_ele(e){
+    var text = e.target.value.toLowerCase();
+    var itemList = items.getElementsByTagName('li')
+    //convert to an array
+    Array.from(itemList)
+    .forEach(function(item){
+        var itemname = item.childNodes[1].textContent;
+        var itemDesc = item.childNodes[0].textContent;
+        if(itemname.toLowerCase().indexOf(text) != -1 || itemDesc.toLowerCase().indexOf(text) !== -1){
+            item.style.display = 'block'
+        }else {
+            item.style.display = 'none'
+        }
+    }); 
+}
