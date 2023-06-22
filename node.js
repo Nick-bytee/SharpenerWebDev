782574241426fa975839437185b0328d1e694eb5
nick_bytee <nikhilkaushik2228@gmail.com>
2023-06-22 22:02:49 +0530

    creating server and printing name

--- node.js
+++ node.js
@@ -1,1 +1,7 @@
-console.log('Hello World')
+const http = require('http')
+const path = require('path')
+const server = http.createServer((req,res) =>{
+    console.log('Nick')
+})
+
+server.listen(4000,'localhost')
