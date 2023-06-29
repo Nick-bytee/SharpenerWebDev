dcfa18a8ef97f6be6cc9c01c6a56515cd77319f6
nick_bytee <nikhilkaushik2228@gmail.com>
2023-06-29 14:46:31 +0530

    form data using express body-parser

--- expressJS/express.js
+++ expressJS/express.js
@@ -1,14 +1,21 @@
 const exp = require('express')
-
 const app = exp()
+const bodyParser = require('body-parser')
 
-app.use((req,res,next) => {
-     console.log('Hello world');
-     next()
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use('/add-product',(req,res) =>{
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "productTitle" placeholder = "Title"></input><input type = "text" name = "quantity" placeholder = "Size"></input><button type = "submit">Add Product</button></form>')
 })
 
-app.use((req,res,next) =>{
-    res.send({'name' : 'Nick'})
+app.post('/product',(req,res) =>{
+    console.log(req.body)
 })
+
+app.use('/',(req,res,next) => {
+    console.log('Welcome To Express JS Project');
+    res.send('<h1>This is home Page</h1><form action = "/add-product" method = "POST"><button type = "submit">Add Product</button></form>')
+})
+
 
 app.listen(3001)
