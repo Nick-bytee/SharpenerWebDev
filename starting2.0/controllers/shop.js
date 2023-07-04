27d76340eac35e5bb209afc085d00450c4837975
nick_bytee <nikhilkaushik2228@gmail.com>
2023-07-04 23:15:56 +0530

    Ecommerce 2.0 ADded dynamic routes

---
+++ starting2.0/controllers/shop.js
@@ -0,0 +1,53 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getProductData = (req,res) =>{
+  const id = req.params.productID
+  Product.findProduct(id,product => {
+  res.render("shop/product-detail",{
+    product : product,
+    pageTitle: product.title,
+    path : '/products'})
+  })
+  // res.redirect('/')
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
