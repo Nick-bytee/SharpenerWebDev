c3b34478de585c1feace3700c2dfdae3750d194c
nickbytee <nikhikaushik2228@gmail.com>
2023-08-04 15:55:02 +0530

    Added per user data

---
+++ ExpenseTracker/backend/controllers/expense.js
@@ -0,0 +1,53 @@
+const Expense = require('../models/expense')
+const jwt = require('jsonwebtoken')
+
+exports.storeData = (req, res) => {
+    const user = jwt.verify(req.body.token, 'secretkey')
+    Expense.create({
+        amount: req.body.amnt,
+        description: req.body.desc,
+        category: req.body.cate,
+        userId: user.userID
+    }).then(data =>
+        res.status(200).json(data)
+    ).catch(err => console.log(err))
+}
+
+exports.getData = async (req, res) => {
+    const user = req.user
+    try {
+        const expenses = await Expense.findAll({where : {
+            userId : user.id
+        }})
+        res.status(200).json(expenses)
+    }catch(err) {
+        console.log(err)
+    }
+}
+
+exports.deleteData = (req, res) => {
+    Expense.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(data =>
+        res.status(200).json(data)
+    ).catch(err => console.log(err))
+}
+
+exports.updateExpense = (req, res) => {
+    const id = req.params.uid
+    console.log(req.body.amount)
+    Expense.update({
+            amount: req.body.amount,
+            description: req.body.description,
+            category: req.body.category
+        }, {
+            where: {
+                id: id
+            }
+        })
+        .then(data =>
+            res.status(200).json(data)
+        ).catch(err => console.log(err))
+}
--- ExpenseTracker/backend/routes/expense.js
+++ ExpenseTracker/backend/routes/expense.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const mainController = require('../controllers/main')
+const mainController = require('../controllers/expense')
+const authController = require('../middleware/auth')
 
 
 router.post('/addExpense', mainController.storeData)
 
-router.get('/getExpenses', mainController.getData)
+router.get('/getExpenses', authController.authenticate, mainController.getData)
 
 router.delete('/deleteExpense/:id', mainController.deleteData)
 
